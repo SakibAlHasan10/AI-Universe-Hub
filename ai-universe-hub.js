eebe0f9d08a137b5e18314bb36fe642fe58d1f43
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-08-30 10:41:52 +0600

    added all card

--- ai-universe-hub.js
+++ ai-universe-hub.js
@@ -11,6 +11,31 @@ const aiToolsCall = (tools) =>{
     tools.tools.forEach(tool => {
         console.log(tool)
         // tool = tool.slice(0,6)
+        const toolCardContainer = document.getElementById('tool-card-container')
+        const toolCard = document.createElement('div');
+        toolCard.innerHTML= `
+        <div class="card w-96 bg-base-100 border-solid border-2 border-slate-400 ">
+                <figure class=" h-52"><img src="${tool?.image || 'Image not available'}" class="h-full w-full"/></figure>
+                <div class="card-body">
+                  <h2 class="card-title">Features</h2>
+                  <p>2. ${tool?.features[1] || 'not found'}</p>
+                  <p>1. ${tool?.features[0] || 'not found'}</p>
+                  <p>3. ${tool?.features[2] || 'not found'}</p>
+                  <hr class="my-5 border-slate-400"/>
+                  <div class="flex justify-between">
+                  <div >
+                  <h2 class="card-title">${tool.name}</h2>
+                  <p>${tool?.published_in || 'not found'}</p>
+                  </div>
+                  <div class="card-actions justify-end">
+                    <button class="btn rounded-full bg-white"><i class="text-xl text-red-500 fa-solid fa-arrow-right"></i></button>
+                  </div>
+                  </div>
+                  
+                </div>
+              </div>
+        `
+        toolCardContainer.appendChild(toolCard)
     });
     // console.log('hello', tools.tools)
 
