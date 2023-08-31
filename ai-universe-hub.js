2d517b9e215851bf7915062d8e73cdc17a61b536
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-08-31 08:40:39 +0600

    add modal

--- ai-universe-hub.js
+++ ai-universe-hub.js
@@ -29,10 +29,10 @@ const aiToolsCall = (tools, isShow) => {
     toolCardContainer.innerHTML = "";
   }
   tools.forEach((tool) => {
-    console.log(tool);
+    // console.log(tool);
     const toolCard = document.createElement("div");
     toolCard.innerHTML = `
-        <div class="card w-96 bg-base-100 border-solid border-2 border-slate-400 ">
+        <div class="card  bg-base-100 border-solid border-2 border-slate-400 ">
                 <figure class=" h-52"><img src="${
                   tool?.image || "Image not available"
                 }" class="h-full w-full"/></figure>
@@ -74,21 +74,52 @@ const cardOfModal = async (id) =>{
 const modalContainer = document.getElementById('modal-container');
 const myModalBody = (aiTool) =>{
     modalContainer.innerHTML=`
-        <div class="flex p-14 gap-6">
-            <div class="flex-1 p-5 border-solid border-2 border-slate-400">
-                <h3 class="font-bold text-lg">Hello!</h3>
-                <p class="py-4">Press ESC key or click on ✕ button to close</p>
+        <div class="md:flex p-12 gap-6">
+            <div class="flex-1 p-5 border-solid border-2 border-slate-400 bg-red-50">
+                <h3 class="font-bold text-lg">${aiTool.description}</h3>
+            <div class="text-center grid grid-cols-3 items-center my-4 gap-3">
+                <div class="bg-white p-4 gap-5 rounded-xl text-green-700 font-semibold ">
+                    <p>${aiTool.pricing[0].price}</p>
+                    <p>${aiTool.pricing[0].plan}</p>
+                </div>
+                <div class="bg-white p-4 gap-5 rounded-xl  font-semibold  text-orange-600 ">
+                    <p>${aiTool.pricing[1].price}</p>
+                    <p>${aiTool.pricing[1].plan}</p>
+                </div>
+                <div class="bg-white p-4 gap-5 rounded-xl text-red-600 font-semibold ">
+                    <p>Contact us</p>
+                    <p>Enterprise</p>
+                </div>
             </div>
-            <div class="flex-1 text-center p-5 border-solid border-2 border-slate-400">
+            <div class=" flex text-left my-4 gap-8 justify-between">
+                <div class="flex-1">
+                    <h2 class="font-bold text-2xl mb-3">Features</h2>
+                    <ol>
+                        <li>${aiTool.features[1].feature_name}</li>
+                        <li>${aiTool.features[2].feature_name}</li>
+                        <li>${aiTool.features[3].feature_name}</li>
+                    </ol>
+                </div>
+                <div class="flex-1">
+                      <h2 class="font-bold text-2xl mb-3">Integrations</h2>
+                      <ul>
+                        <li>${aiTool.integrations[0]}</li>
+                        <li>${aiTool.integrations[1]}</li>
+                        <li>${aiTool.integrations[2]}</li>
+                    </ul>
+                </div>
+            </div>
+            </div>
+            <div class="flex-1 text-center p-5 pb-0 border-solid border-2 border-slate-400">
                 <img src="${aiTool.image_link[0]}" class="h-56 mb-4" />
                 <h2 class="text-xl font-semibold">${aiTool.input_output_examples[0].input}</h2>
                 <p class="text-sm font-normal mt-3">${aiTool.input_output_examples[0].output}</p>
             </div>
-            
         </div>
         
     `
-    console.log(aiTool)
+    // console.log(aiTool)
+    // console.log(aiTool.features[1].feature_name)
     modalId()
 }
 const loading = document.getElementById("loading");
@@ -106,6 +137,6 @@ const modalId = () =>{
 }
 const showAllCard = (isShow) => {
   allAiData(isShow);
-  console.log("hello", isShow);
+  // console.log("hello", isShow);
 };
 allAiData();
